bd67c8354741fbe3251a98a5234313c860a0b422
RodroVMS <democlesvanshepars@gmail.com>
2021-11-22 14:01:18 -0500

    initial commit

---
+++ src/agent.ts
@@ -0,0 +1,45 @@
+import BigNumber from 'bignumber.js'
+import {
+    CALL_EXECUTED_EVENT,
+    CALL_SCHEDULED_EVENT,
+    MIN_DELAY_CHANGED_EVENT, 
+}from "./constants" 
+import { 
+  BlockEvent, 
+  Finding, 
+  HandleBlock, 
+  HandleTransaction, 
+  TransactionEvent, 
+  FindingSeverity, 
+  FindingType 
+} from 'forta-agent'
+
+
+const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
+  const findings: Finding[] = []
+
+  // create finding if gas used is higher than threshold
+  const gasUsed = new BigNumber(txEvent.gasUsed)
+  if (gasUsed.isGreaterThan("1000000")) {
+    findings.push(Finding.fromObject({
+      name: "High Gas Used",
+      description: `Gas Used: ${gasUsed}`,
+      alertId: "FORTA-1",
+      severity: FindingSeverity.Medium,
+      type: FindingType.Suspicious
+    }))
+  }
+
+  return findings
+}
+
+// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+//   const findings: Finding[] = [];
+//   // detect some block condition
+//   return findings;
+// }
+
+export default {
+  handleTransaction,
+  // handleBlock
+}
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const TIMELOCK_CONTROLLER_ADDRESS = "0x92103934D64299Dd952F88d747ABBD914fC87f74"
+export const CALL_SCHEDULED_EVENT = "evetn CallScheduled(bytes32 indexed id, uint256 indexed index, address target, uint256 value, bytes data, bytes32 predecessor, uint256 delay)";
+export const CALL_EXECUTED_EVENT = "event CallExecuted(bytes32 indexed id, uint256 indexed index, address target, uint256 value, bytes data)";
+export const MIN_DELAY_CHANGED_EVENT = "event MinDelayChange(uint256 oldDuration, uint256 newDuration)";
